f81106c13010e495408cca5ef89cf232337e7c61
Alan Ford <alanford354@gmail.com>
2021-12-09 14:46:00 -0500

    Added a Controller class

--- firmware/stm32HAL/f746/Project/Inc/controller.hpp
+++ firmware/stm32HAL/f746/Project/Inc/controller.hpp
@@ -8,8 +8,27 @@
 #ifndef INC_CONTROLLER_HPP_
 #define INC_CONTROLLER_HPP_
 
-int get_left_PID_Voltage_Value();
-int get_right_PID_Voltage_Value();
+#include "motor.hpp"
+#include "imu.hpp"
 
+//int get_left_PID_Voltage_Value();
+//int get_right_PID_Voltage_Value();
 
+class Controller {
+	float           angle_Kp;
+	float           angle_Ki;
+	float           angle_Kd;
+	float           angle_Ks;
+	float     		friction_Value;
+	Motor *motor;
+	IMU *imu;
+public:
+	Controller(float Kp, float Ki, float Kd, float Ks, float friction = 10.);
+	int get_PID_Voltage_Value();
+	void set_Kp(float value);
+	void set_Ki(float value);
+	void set_Kd(float value);
+	void set_Ks(float value);
+	void set_friction(float value);
+};
 #endif /* INC_CONTROLLER_HPP_ */
--- firmware/stm32HAL/f746/Project/Src/controller.cpp
+++ firmware/stm32HAL/f746/Project/Src/controller.cpp
@@ -14,7 +14,7 @@
 #include <motor.hpp>
 #include "imu.hpp"
 #define constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
-
+/*
 float           theta_Kp = 1200;
 float           theta_Ki = 0;
 float           theta_Kd = 130;
@@ -49,8 +49,6 @@ int get_left_PID_Voltage_Value() {
 }
 
 
-
-
 int get_right_PID_Voltage_Value() {
 
   float right_P_Accel = omega_Kp * (omega_Now - omega_Zero);
@@ -72,4 +70,49 @@ int get_right_PID_Voltage_Value() {
   int right_PID_Voltage = round(constrain(voltage, -voltage_Max, voltage_Max));
   return right_PID_Voltage;
 }
+*/
+
+Controller::Controller(float Kp, float Ki, float Kd, float Ks, float friction):
+		angle_Kp(Kp), angle_Ki(Ki), angle_Kd(Kd), angle_Ks(Ks), friction_Value(friction) {
+}
+
+
+int Controller::get_PID_Voltage_Value(){
+	  float P_Accel = angle_Kp * (angle_Now - angle_Zero);
+	  float I_Accel = angle_Ki * angle_Integral;
+	  float D_Accel = angle_Kd * angle_Speed_Now;
+	  float S_Accel = angle_Ks * speed_RPM / 1000.;
+	  float PID_Accel = P_Accel + I_Accel + D_Accel + S_Accel;
+
+	  float friction = 0;
+	  if (speed_RPM > 0.5) {
+	    friction = friction_Value;
+	  } else if (speed_RPM < -0.5) {
+	    friction = -friction_Value;
+	  }
+
+	  float voltage = (PID_Accel + 0.30 * speed_RPM + friction) / 9.4;     // Equation measured from Acceleration Motor Tests
+	  int voltage_limit = motor->get_max_voltage();
+	  int PID_Voltage = round(constrain(voltage, -voltage_limit, voltage_limit));
+	  return PID_Voltage;
+}
+
+void Controller::set_Kp(float value){
+	angle_Kp=value;
+}
+void Controller::set_Ki(float value){
+	angle_Ki=value;
+}
+void Controller::set_Kd(float value){
+	angle_Kd=value;
+}
+void Controller::set_Ks(float value){
+	angle_Ks=value;
+}
+void Controller::set_friction(float value){
+	friction_Value=value;
+}
+
+
+
 
