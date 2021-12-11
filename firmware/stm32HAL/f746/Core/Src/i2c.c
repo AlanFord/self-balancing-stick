b60944474faa9dd39f0dd31072c9d1d8b2405ccf
Alan Ford <alanford354@gmail.com>
2021-12-11 13:19:28 -0500

    set the I2c rise and fall times to non-zero values

--- firmware/stm32HAL/f746/Core/Src/i2c.c
+++ firmware/stm32HAL/f746/Core/Src/i2c.c
@@ -38,7 +38,7 @@ void MX_I2C1_Init(void)
 
   /* USER CODE END I2C1_Init 1 */
   hi2c1.Instance = I2C1;
-  hi2c1.Init.Timing = 0x00808CD2;
+  hi2c1.Init.Timing = 0x60311419;
   hi2c1.Init.OwnAddress1 = 0;
   hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
   hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
