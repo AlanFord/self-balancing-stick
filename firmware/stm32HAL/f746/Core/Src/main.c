03525bcb6371aa5e973388bafeecd98508400679
Alan Ford <alanford354@gmail.com>
2021-12-11 19:47:10 -0500

    two bug fixes

--- firmware/stm32HAL/f746/Core/Src/main.c
+++ firmware/stm32HAL/f746/Core/Src/main.c
@@ -106,6 +106,7 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	  app_entry();
   }
   /* USER CODE END 3 */
 }
--- firmware/stm32HAL/f746/Project/Inc/terminal.h
+++ firmware/stm32HAL/f746/Project/Inc/terminal.h
@@ -7,7 +7,18 @@
 #ifndef __TERMINAL_H__
 #define __TERMINAL_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
 void Terminal_Init(void);
 int_fast8_t Terminal_Process(void);
 void DisplaySystemInformation(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+
 #endif /* __TERMINAL_H__ */
